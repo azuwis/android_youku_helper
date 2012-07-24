f7572a4e05772f6eb266ff179172801297a8d65e
Zhong Jianxin (azuwis) <azuwis@gmail.com>
2012-07-24 15:46:46 +0800

    call MainActivity from YoukuHelperActivity, and pass video list to it

--- src/name/azuwis/webhelper/MainActivity.java
+++ src/name/azuwis/webhelper/MainActivity.java
@@ -1,17 +1,72 @@
 package name.azuwis.webhelper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+
+
+    private static final String TAG = "MainActivity";
+    public static final String PREFS_VIDEOLIST = "VideoList";
+
+    private ListView mainListView;
+    public static ArrayAdapter<String> listAdapter;
+    private WebView webview;
+    private int listPosition = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webview = new WebView(this);
+        mainListView = (ListView) findViewById( R.id.mainListView );
+        listAdapter = new ArrayAdapter<String>(this, R.layout.simple_row);
+        mainListView.setAdapter( listAdapter );
+
+        mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            //@Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                listPosition = position + 1;
+                Object link = mainListView.getItemAtPosition(position);
+                //String link = ((TextView)view).getText().toString();
+                webview.loadUrl((String)link);
+                Log.d(TAG, "position: "+ position);
+            }
+        });
+        ArrayList<String> linkList = getIntent().getStringArrayListExtra("linkList");
+        if (linkList == null) {
+            //TODO get linkList from data storage
+        }
+        updateView(linkList);
+    }
+
+    @Override
+	protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        ArrayList<String> linkList = intent.getStringArrayListExtra("linkList");
+        updateView(linkList);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (listPosition > 0 && listPosition < mainListView.getCount()) {
+          Object item = mainListView.getItemAtPosition(listPosition);
+          listPosition++;
+          webview.loadUrl((String)item);
+        }
     }
 
     @Override
@@ -19,5 +74,14 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
-    
+
+    private void updateView(List<String> linkList) {
+        listPosition = 0;
+        if (linkList != null) {
+            listAdapter.clear();
+            for (String link : linkList) {
+                listAdapter.add(link);
+            }
+        }
+    }
 }
--- src/name/azuwis/webhelper/YoukuHelperActivity.java
+++ src/name/azuwis/webhelper/YoukuHelperActivity.java
@@ -9,16 +9,9 @@ import java.util.List;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.View;
-import android.webkit.WebView;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import org.jsoup.Jsoup;
@@ -29,64 +22,24 @@ import org.jsoup.select.Elements;
 public class YoukuHelperActivity extends Activity {
 
     private static final String TAG = "YoukuHelperActivity";
-
-    private ListView mainListView;
-    private ArrayAdapter<String> listAdapter;
-    private WebView webview;
-    private int listPosition = 0;
+    public static final String PREFS_VIDEOLIST = "VideoList";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        webview = new WebView(this);
-        mainListView = (ListView) findViewById( R.id.mainListView );
-        listAdapter = new ArrayAdapter<String>(this, R.layout.simple_row);
-        mainListView.setAdapter( listAdapter );
-
-        mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            //@Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                listPosition = position + 1;
-                Object link = mainListView.getItemAtPosition(position);
-                //String link = ((TextView)view).getText().toString();
-                webview.loadUrl((String)link);
-                Log.d(TAG, "position: "+ position);
-            }
-        });
-        callFlvcd(getIntent());
+        callFlvcd(getIntent().getData().toString());
     }
 
-    @Override
-	protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-        callFlvcd(intent);
-    }
+    private void callFlvcd(String url) {
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (listPosition > 0 && listPosition < mainListView.getCount()) {
-          Object item = mainListView.getItemAtPosition(listPosition);
-          listPosition++;
-          webview.loadUrl((String)item);
-        }
-    }
-
-    private void callFlvcd(Intent intent) {
-        listPosition = 0;
-        Uri webpage = intent.getData();
-
-        String url = "";
-
+        String flvUrl = "";
         try {
-            url = "http://www.flvcd.com/parse.php?flag=one&format=super&kw=" + URLEncoder.encode(webpage.toString(), "UTF-8");
+            flvUrl = "http://www.flvcd.com/parse.php?flag=one&format=super&kw=" + URLEncoder.encode(url, "UTF-8");
         } catch (UnsupportedEncodingException e) {
             Log.d(TAG, "err: " + e.getMessage());
         }
 
-        new ParseFlvcd().execute(url);
+        new ParseFlvcd().execute(flvUrl);
     }
 
     private class ParseFlvcd extends AsyncTask<String, Object, List<String>> {
@@ -123,14 +76,16 @@ public class YoukuHelperActivity extends Activity {
         @Override
         protected void onPostExecute(List<String> linkList) {
             dialog.dismiss();
-            listAdapter.clear();
             if(linkList.isEmpty()) {
                 Toast toast = Toast.makeText(YoukuHelperActivity.this, "Error when getting video links, try again later", Toast.LENGTH_SHORT);
                 toast.show();
             }
-            for (String link : linkList) {
-                listAdapter.add(link);
-            }
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            intent.putStringArrayListExtra("linkList", (ArrayList<String>)linkList);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            startActivity(intent);
+            finish();
+            overridePendingTransition(0, 0);
         }
     }
 }
